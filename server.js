5e23ae0bafe53fa5fd547204e500a24a974c06d0
Corina Andariese <candariese@gmail.com>
2018-08-03 13:05:16 -0700

    Added logger, router, and body parser middleware for koa.

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
-const koa = require('koa');
+const koa = require('koa')
+const logger =  require('koa-morgan')
+const Router = require('koa-router')
+const bodyParser = require('koa-body')()
 
-const server = new koa();
+const server = new koa()
+const router = new Router()
+
+router.get('/', ctx => {
+  ctx.body ='I AM ROOT! LULZ'
+});
+
+router.get('/second_route', ctx => {
+  ctx.body = 'I AM second_route'
+})
+
+router.post('/something', ctx => {
+  ctx.body = {
+    something: 'something here'
+  }
+})
 
 server
-  .use(ctx => {
-  ctx.body = 'I AM YOUR FIRST KOA.JS API!!'
-})
-.listen(3001)
+  .use(logger('tiny'))
+  .use(router.routes())
+  .listen(3001)
