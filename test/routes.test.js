31b601d3653e432a7a98fb50e3fdd79d291026df
Corina Andariese <candariese@gmail.com>
2018-08-05 11:58:00 -0700

    Added first route test

---
+++ test/routes.test.js
@@ -0,0 +1,24 @@
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const should = chai.should();
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+const server = require('../server');
+
+describe('routes: server', () => {
+	describe('GET /', () => {
+		it('should return json', (done) => {
+			chai
+				.request(server)
+				.get('/')
+				.end((err, res, next) => {
+					should.not.exist(err);
+					res.status.should.eql(200);
+					res.body.status.should.equal('success!');
+					done();
+				});
+		});
+	});
+});
