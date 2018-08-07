86d6f0a68b23dd3ef6301a16bdb74e38e551d361
Corina Andariese <candariese@gmail.com>
2018-08-07 14:45:29 -0700

    added new env variables to knexfile

---
+++ knexfile.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const BASE_PATH = path.join(__dirname, 'src', 'server', 'db');
+require('dotenv').config();
+
+module.exports = {
+	test: {
+		client: 'postgres',
+		connection: process.env.DB_CONNECTION,
+		migrations: {
+			directory: path.join(BASE_PATH, 'migrations')
+		},
+		seeds: {
+			directory: path.join(BASE_PATH, 'seeds')
+		}
+	},
+
+	development: {
+		client: 'postgres',
+		connection: process.env.DB_CONNECTION,
+		migrations: {
+			directory: path.join(BASE_PATH, 'migrations')
+		},
+		seeds: {
+			directory: path.join(BASE_PATH, 'seeds')
+		}
+	}
+};
