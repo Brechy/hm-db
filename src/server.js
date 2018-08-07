c9cf5f8a1a19a16fcee8eabb624a29586c602653
Corina Andariese <candariese@gmail.com>
2018-08-07 14:42:47 -0700

    require dotenv

--- src/server.js
+++ src/server.js
@@ -2,11 +2,15 @@ const Koa = require('koa');
 const logger = require('koa-morgan');
 const bodyParser = require('koa-body')();
 const usersRouter = require('./server/routes/users');
+const cardsRouter = require('./server/routes/cards');
+
+require('dotenv').config();
 
 const app = new Koa();
 const PORT = process.env.PORT || 1337;
 
 app.use(usersRouter.routes());
+app.use(cardsRouter.routes());
 
 const server = app.listen(PORT, () => {
 	console.log(`Server listening on ${PORT}`);
