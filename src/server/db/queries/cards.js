88b6fdeb1b186230c19bfe33ade2d099c0fcc221
Corina Andariese <candariese@gmail.com>
2018-08-15 10:33:39 -0700

    Query for getSingleCard

--- src/server/db/queries/cards.js
+++ src/server/db/queries/cards.js
@@ -5,11 +5,18 @@ addSingleCard = (card) => {
 	return knex('cards').insert(card);
 };
 
+getSingleCard = (card) => {
+	return knex('cards')
+		.select('*')
+		.where({ id: parseInt(id) });
+};
+
 getAllCards = () => {
 	return knex('cards').select('*');
 };
 
 module.exports = {
 	addSingleCard,
+	getSingleCard,
 	getAllCards
 };
