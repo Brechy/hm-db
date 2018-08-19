93a092e8ecf651070f0058c104fc2d03ce3c0655
Corina Andariese <candariese@gmail.com>
2018-08-18 22:35:27 -0700

    removed id from cache seed file

--- src/server/db/seeds/cache_seed.js
+++ src/server/db/seeds/cache_seed.js
@@ -5,35 +5,30 @@ exports.seed = (knex, Promise) => {
 		.then(() => {
 			// Inserts seed entries
 			return knex('hangul_cache').insert({
-				id: 1,
 				english: 'Hello',
 				hangul: '안녕하세요',
 			});
 		})
 		.then(() => {
 			return knex('hangul_cache').insert({
-				id: 2,
 				english: 'I love you',
 				hangul: '사랑해',
 			});
 		})
 		.then(() => {
 			return knex('hangul_cache').insert({
-				id: 3,
 				english: 'Thank you',
 				hangul: '감사합니다',
 			});
 		})
     .then(() => {
 			return knex('hangul_cache').insert({
-				id: 4,
 				english: 'Dog',
 				hangul: '개',
 			});
 		})
     .then(() => {
 			return knex('hangul_cache').insert({
-				id: 5,
 				english: 'Cat',
 				hangul: '고양이',
 			});
