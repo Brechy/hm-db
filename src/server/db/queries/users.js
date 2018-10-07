e68f2821a3be1b8e46fd6933bb56f6d9fa13a8ef
Brechin <brechin@brechy.net>
2018-10-06 20:27:00 -0700

    Added validation query for users

--- src/server/db/queries/users.js
+++ src/server/db/queries/users.js
@@ -1,10 +1,56 @@
 const knex = require('../connection');
+const bcrypt = require('bcrypt');
 
-addSingleUser = (user) => {
+const validateUser = async (username, password) => {
+	//hash password
+	const hashedPassword = await new Promise((resolve, reject) => {
+		bcrypt.hash(password, 9, function(err, hash) {
+			if(err) {
+				reject(err)
+				return
+			}
+			resolve(hash)
+		})
+	})
+	const legit = await knex('users')
+	.select('*')
+	.where({
+		username: username,
+		password: hashedPassword,
+	})
+	console.log(legit)
+}
+
+const addSingleUser = async (user) => {
+
+  const password = user.password;
+	const username = user.username;
+	console.log("I am the password", password)
+	console.log("I am the username", username)
+
+  const hashedPass = await new Promise((resolve, reject) => {
+		bcrypt.hash(password, 9, function(err, hash) {
+			if(err) {
+				console.log("Error hashing password", err)
+				reject(err)
+				return
+			}
+			resolve(hash)
+	  })
+	})
+	const userInfo = {
+		username: username, password: hashedPass
+	}
 	return knex('users')
-		.insert(user)
-		.returning('*');
+	.insert(userInfo)
+	.returning('*');
 };
+
+getSingleUser = (username) => {
+  return knex('users')
+		.select('*')
+		.where({username: username});
+}
 
 getAllUsers = () => {
 	return knex('users').select('*');
