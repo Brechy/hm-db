496038c3108bfca45b7a5481e0dec184c526651a
Brechin <brechin@brechy.net>
2018-10-06 20:27:27 -0700

    Add login routes for authentication and authorization

--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -3,38 +3,65 @@ const queries = require('../db/queries/users');
 
 const router = new Router();
 
+//post login user
+router.post('/login', async (ctx) => {
+  try {
+    const valid = await queries.validateUser();
+    if (!valid) {
+      ctx.body = {
+        status: 'failure'
+      }
+      return
+    }
+    //generate jwt
+		const cert = process.env.PRIVATE_KEY
+    const jwt = jwt.sign({ username:user.username }, cert, {
+      algorithm: 'RS256'
+    },
+		function(err, token) {
+      console.log(token);
+    });
+    ctx.body = {
+      status: 'success',
+      data: jwt
+    }
+  } catch (err) {
+    console.log(err)
+  }
+})
+
 router.get('/users', async (ctx) => {
-	try {
-		const users = await queries.getAllUsers();
-		ctx.body = {
-			status: 'success',
-			data: users
-		};
-	} catch (err) {
-		console.log(err);
-	}
+  try {
+    const users = await queries.getAllUsers();
+    ctx.body = {
+      status: 'success',
+      data: users
+    };
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 router.post('/users', async (ctx) => {
-	try {
-		const body = ctx.request.body;
-		console.log(ctx.request.body)
-		const users = await queries.addSingleUser(ctx.request.body);
-		if (users.length) {
-			ctx.status = 201;
-			ctx.body = {
-				status: 'success',
-			};
-		} else {
-			ctx.status = 400;
-			ctx.body = {
-				status: 'error',
-				message: `Something went wrong ${JSON.stringify(users)}`
-			};
-		}
-	} catch (err) {
-		console.log(err);
-	}
+  try {
+    const body = ctx.request.body;
+    console.log(ctx.request.body)
+    const users = await queries.addSingleUser(ctx.request.body);
+    if (users.length) {
+      ctx.status = 201;
+      ctx.body = {
+        status: 'success'
+      };
+    } else {
+      ctx.status = 400;
+      ctx.body = {
+        status: 'error',
+        message: `Something went wrong ${JSON.stringify(users)}`
+      };
+    }
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
