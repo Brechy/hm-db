377384553d4f7e0f2686a002657e2902ea6881fd
Carl Corsini <carlcorsini@Carls-MacBook-Air.local>
2018-10-16 14:39:52 -0700

    tests passing

--- knexfile.js
+++ knexfile.js
@@ -1,37 +1,45 @@
-const path = require('path');
-const BASE_PATH = path.join(__dirname, 'src', 'server', 'db');
-require('dotenv').config();
+const path = require('path')
+const BASE_PATH = path.join(__dirname, 'src', 'server', 'db')
+require('dotenv').config()
+
+console.log(process.env.DB_CONNECTION)
 
 module.exports = {
-	test: {
-		client: 'postgres',
-		connection: 'localhost',
-		migrations: {
-			directory: path.join(BASE_PATH, 'migrations')
-		},
-		seeds: {
-			directory: path.join(BASE_PATH, 'seeds')
-		}
-	},
+  test: {
+    client: 'postgres',
+    connection: {
+      host: 'localhost',
+      database: 'hm-db_test'
+    },
+    migrations: {
+      directory: path.join(BASE_PATH, 'migrations')
+    },
+    seeds: {
+      directory: path.join(BASE_PATH, 'seeds')
+    }
+  },
 
-	development: {
-		client: 'postgres',
-		connection: process.env.DB_CONNECTION,
-		migrations: {
-			directory: path.join(BASE_PATH, 'migrations')
-		},
-		seeds: {
-			directory: path.join(BASE_PATH, 'seeds')
-		}
-	},
-	production: {
-		client: 'postgres',
-		connection: process.env.DB_CONNECTION,
-		migrations: {
-			directory: path.join(BASE_PATH, 'migrations')
-		},
-		seeds: {
-			directory: path.join(BASE_PATH, 'seeds')
-		}
-	}
-};
+  development: {
+    client: 'postgres',
+    connection: {
+      host: 'localhost',
+      database: 'hm-db'
+    },
+    migrations: {
+      directory: path.join(BASE_PATH, 'migrations')
+    },
+    seeds: {
+      directory: path.join(BASE_PATH, 'seeds')
+    }
+  },
+  production: {
+    client: 'postgres',
+    connection: process.env.DB_CONNECTION,
+    migrations: {
+      directory: path.join(BASE_PATH, 'migrations')
+    },
+    seeds: {
+      directory: path.join(BASE_PATH, 'seeds')
+    }
+  }
+}
--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -1,15 +1,17 @@
-const Router = require('koa-router');
-const queries = require('../db/queries/users');
-const jwt = require('jsonwebtoken');
+const Router = require('koa-router')
+const queries = require('../db/queries/users')
+const jwt = require('jsonwebtoken')
 
-const router = new Router();
+const router = new Router()
 
 //post login user
-router.post('/login', async (ctx) => {
+
+router.post('/login', async ctx => {
   console.log('>>> This is me trying to post to /login')
+
   try {
-		const body = ctx.request.body;
-    const valid = await queries.validateUser(body.username, body.password);
+    const body = ctx.request.body
+    const valid = await queries.validateUser(body.username, body.password)
     if (!valid) {
       ctx.body = {
         status: 'failure'
@@ -17,11 +19,9 @@ router.post('/login', async (ctx) => {
       return
     }
     //generate jwt
-		const cert = process.env.PRIVATE_KEY
+    // const cert = process.env.PRIVATE_KEY
 
-		const supesLegit = jwt.sign({ username: body.username }, cert, {
-      algorithm: 'RS256'
-    });
+    const supesLegit = jwt.sign({ username: body.username }, 'hey')
     ctx.body = {
       status: 'success',
       data: supesLegit
@@ -31,38 +31,38 @@ router.post('/login', async (ctx) => {
   }
 })
 
-router.get('/users', async (ctx) => {
+router.get('/users', async ctx => {
   try {
-    const users = await queries.getAllUsers();
+    const users = await queries.getAllUsers()
     ctx.body = {
       status: 'success',
       data: users
-    };
+    }
   } catch (err) {
-    console.log(err);
+    console.log(err)
   }
-});
+})
 
-router.post('/users', async (ctx) => {
+router.post('/users', async ctx => {
   try {
-    const body = ctx.request.body;
+    const body = ctx.request.body
     console.log(ctx.request.body)
-    const users = await queries.addSingleUser(ctx.request.body);
+    const users = await queries.addSingleUser(ctx.request.body)
     if (users.length) {
-      ctx.status = 201;
+      ctx.status = 201
       ctx.body = {
         status: 'success'
-      };
+      }
     } else {
-      ctx.status = 400;
+      ctx.status = 400
       ctx.body = {
         status: 'error',
         message: `Something went wrong ${JSON.stringify(users)}`
-      };
+      }
     }
   } catch (err) {
-    console.log(err);
+    console.log(err)
   }
-});
+})
 
-module.exports = router;
+module.exports = router
